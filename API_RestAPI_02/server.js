77dcf6fcbf92c8b611c4f7652e38f9ed6087dd95
jeelumHere <syedsharjeelahmed214@gmail.com>
2026-06-13 23:32:12 +0500

    using get method

--- API_RestAPI_02/server.js
+++ API_RestAPI_02/server.js
@@ -6,13 +6,3 @@ app.listen(3000,()=>{
     console.log("server is running on port 3000");
 })
 
-const note = []
-
-app.post("/note",(req,res)=>{
-    console.log(req.body);
-    note.push(req.body);
-
-    res.status(201).json({
-        message : "note created successfully"
-    })
-})
--- API_RestAPI_02/src/app.js
+++ API_RestAPI_02/src/app.js
@@ -6,4 +6,23 @@ const app = express() // server instant is created
 
 app.use(express.json()); // using middleware
 
+const note = []
+
+app.post("/note",(req,res)=>{
+    console.log(req.body);
+    note.push(req.body);
+
+    res.status(201).json({
+        message : "note created successfully"
+    })
+})
+
+app.get("/note",(req,res)=>{
+    res.status(200).json({
+        message : "Notes fethed successfully",
+        note : note
+    })
+})
+
+
 module.exports = app

d81c7fbb1130f7223280ee2c5e56f58daf121469
jeelumHere <syedsharjeelahmed214@gmail.com>
2026-06-20 23:18:08 +0500

    Note_Model

--- Database_02/src/app.js
+++ Database_02/src/app.js
@@ -1,7 +1,19 @@
 // creates the server
 
 const express = require("express")
+const noteModel= require("./model/note.model")
+
 
 const app = express()
+app.use(express.json())
+
+/*
+POST =>  /Note =>   create a note
+GET  =>  /Note =>   Get all notes
+DELETE=> /Note/:id  Delete a note on server
+PATCH => /NOTE/:id  Update anote on the server
+ */
+
+
 
 module.exports = app

326d4d7cac97d0fe58a1d7ec742788815d37dbe5
jeelumHere <syedsharjeelahmed214@gmail.com>
2026-06-28 22:36:22 +0500

    ReactJs Setup

---
+++ Project_CSP_Integration/Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Hello Reactjs</h1>}/>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default App

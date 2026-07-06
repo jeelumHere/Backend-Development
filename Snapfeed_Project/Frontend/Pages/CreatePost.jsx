6ad4ff5931572808871862151c2dd849817aa6a2
jeelumHere <syedsharjeelahmed214@gmail.com>
2026-07-06 16:39:50 +0500

    Post Deletion API

--- Snapfeed_Project/Frontend/Pages/CreatePost.jsx
+++ Snapfeed_Project/Frontend/Pages/CreatePost.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import axios from "axios"
+import {useNavigate} from "react-router-dom"
+
 
 const CreatePost = () => {
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+
+    const formData = new FormData(e.target)
+
+    axios.post("http://localhost:3000/create-post",formData)
+    .then((res)=>{
+      console.log(res)
+      navigate("/feed")
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
   return (
-    <div>
-      <h1 className='text-4xl font-bold'>I am CreatePost Page</h1>
-    </div>
+    <>
+    
+    <section className='flex flex-col justify-center items-center'>
+
+      <h1 className='text-4xl font-bold mb-2'>Create Post</h1>
+
+      <form onSubmit={handleSubmit} className='bg-amber-100 p-4 mb-2 rounded-2xl flex flex-col gap-2.5' method='/post' action='upload'>
+
+        <label for="images">Select images:</label>
+          <div><input className='bg-white p-2 rounded-md w-full' type="file" multiple placeholder='Select Images'name='images' accept='image/*' /></div>
+
+          <label>Enter Title</label>
+          <div ><input className='bg-white p-2 rounded-md w-full' type="text" name='title' pattern="^\s*(\S+\s*){0,5}$"/></div>
+
+          <label>Enter Caption</label>
+          <div><textarea className='bg-white p-2 w-full rounded-md' name="caption"></textarea></div>
+
+          <button className='p-2 bg-white hover:cursor-pointer font-semibold hover:shadow-amber-600 rounded-2xl' type='submit'>Submit</button>
+
+      </form>
+    </section>
+    </>
   )
 }
 
--- Snapfeed_Project/Frontend/Pages/Feed.jsx
+++ Snapfeed_Project/Frontend/Pages/Feed.jsx
@@ -5,19 +5,32 @@ const Feed = () => {
     const [post, setPost] = useState([])
 
     useEffect(() => {
+
+
         axios.get("http://localhost:3000/post")
             .then(res => {
                 console.log(res.data.Data)
                 setPost(res.data.Data)
             })
             .catch(err => console.error("Error fetching data:", err))
+
     }, [])
+
+    const handleDeletepost = async (postId) => {
+    axios.delete(`http://localhost:3000/post/${postId}`)
+        .then(() => {
+            setPost(prev => prev.filter(e => postId !== e._id))
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 
     return (
         <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-6 p-4 justify-items-center'>
             {post.map((ele) => (
-                <div key={ele._id} className='flex flex-col gap-3 items-center w-full max-w-[500px] bg-gray-400 rounded-3xl p-4 shadow-md'>
-                    <h1 className='text-3xl font-bold text-center break-words w-full'>{ele.title}</h1>
+                <div key={ele._id} className='flex flex-col gap-3 items-center w-full max-w-125 bg-gray-400 rounded-3xl p-4 shadow-md'>
+                    <h1 className='text-3xl font-bold text-center wrap-break-words w-full'>{ele.title}</h1>
 
                     <div className='flex gap-2'>
 
@@ -31,6 +44,7 @@ const Feed = () => {
                     </div>
 
                     <p className='text-center wrap-break-words w-full px-2'>{ele.caption}</p>
+                    <button className='p-2 bg-white rounded-2xl hover:cursor-pointer hover:bg-red-400 hover:text-white duration-200' onClick={() => handleDeletepost(ele._id)} >Delete Post</button>
                 </div>
             ))}
         </div>
